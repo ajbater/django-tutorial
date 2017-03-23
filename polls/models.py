4151ad051f577f57026f4f77b1dd46b03838992f
Amanda Bater <amanda22@hotmail.co.uk>
2017-03-23 14:42:32 +0000

    adds recently added method to questions

--- polls/models.py
+++ polls/models.py
@@ -13,6 +13,10 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 @python_2_unicode_compatible
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
