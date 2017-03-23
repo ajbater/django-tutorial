dfa1d7ce9a196c6b814e570c088a16fd5ed9d53d
Amanda Bater <amanda22@hotmail.co.uk>
2017-03-23 16:00:36 +0000

    tests that only questions whose pub_date is in the past can be displayed in detail view

--- polls/tests.py
+++ polls/tests.py
@@ -8,7 +8,6 @@ from django.urls import reverse
 from .models import Question
 
 class QuestionMethodTests(TestCase):
-
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() should return False for questions whose pub_date is in the future.
@@ -41,7 +40,6 @@ def create_question(question_text, days):
     return Question.objects.create(question_text=question_text, pub_date=time)
 
 class QuestionViewTests(TestCase):
-
     def test_index_view_with_no_questions(self):
         """
         If no questions exist, an appropriate message should be displayed.
@@ -94,3 +92,22 @@ class QuestionViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2>', '<Question: Past question 1>']
         )
+
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future should return a 404 not found.
+        """
+        future_question = create_question(question_text="Future question", days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should display the question's text.
+        """
+        past_question = create_question(question_text="Past question", days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
